82d0260eb42d8ad69d6d1e7dacd601d95ddb471f
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-08 16:53:08 +0600

    simple update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function findSongs(){
+    const searchSongBtn = document.getElementById('search-song-btn').value;
+
+    async function songsSearchResult(){
+        const response =  await fetch('');
+    }
+}
