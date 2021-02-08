c6d957efefe8fca61b898f75dd5b88d5d1d1154c
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-09 02:38:14 +0600

    simple update

--- js/main.js
+++ js/main.js
@@ -87,3 +87,29 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+
+
+// //API connect from search box with validation
+// const findSongs = () => {
+//     const searchSongs = document.getElementById('search-songs').value;
+    
+//     async function songsSearchResult(){
+//         const response =  await fetch('https://api.lyrics.ovh/suggest/'+searchSongs);
+//         const data = await response.json();
+//         return data;
+//     }
+//     if(searchSongs === ""){
+//         // alert('Please enter the song name');
+//         displayError("Please enter the song name!!");
+//     }
+//     else{
+//         songsSearchResult().then(data => {
+//             // console.log(data.data.artist.name);
+//             displaySongs(data.data);
+            
+//         })
+//         .catch(error => displayError("Something went wrong!! Please try again later!"));
+//     }
+    
+// };
