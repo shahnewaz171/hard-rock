747d4940712ca22855b8ee6a5109c8d3e0ea4732
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-09 02:36:22 +0600

    simple update

--- js/main.js
+++ js/main.js
@@ -79,3 +79,11 @@ const displayError = error => {
     const errorTag = document.getElementById('error-message');
     errorTag.innerText = error;
 };
+
+//For Mobile navBar
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
